eae25bc9ac8826c983b6b2eee97e5e048cf5b471
movik71 <42829120+movik71@users.noreply.github.com>
2021-10-28 19:26:32 +0300

    Add files via upload

---
+++ lab4/MidpointDisplacement/MidpointDisplacement/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MidpointDisplacement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawRocks = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DrawRocks
+            // 
+            this.DrawRocks.Location = new System.Drawing.Point(13, 13);
+            this.DrawRocks.Name = "DrawRocks";
+            this.DrawRocks.Size = new System.Drawing.Size(75, 23);
+            this.DrawRocks.TabIndex = 0;
+            this.DrawRocks.Text = "DrawRocks";
+            this.DrawRocks.UseVisualStyleBackColor = true;
+            this.DrawRocks.Click += new System.EventHandler(this.DrawRocks_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "DrawSteps";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DrawRocks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawRocks;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab4/MidpointDisplacement/MidpointDisplacement/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace MidpointDisplacement
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        Random rand;
+        private ArrayList points;
+        
+        private int R = 10;
+
+        void MidpointDisplacement(int xb, int yb, int xe, int ye, int control = 0)
+        {
+            double l = Math.Sqrt((xe - xb) * (xe - xb) + (ye - yb) * (ye - yb));
+            if (Width/2 > Convert.ToInt32(Math.Pow(2, control)))
+            {
+                control++;
+                int xc = (xe + xb) / 2;
+                int yc = ((Height/2 + yb) + (Height/2 + ye)) / 2 + 
+                         rand.Next(Convert.ToInt32(-l / R), Convert.ToInt32(l / R) + 1);
+                yc = -Height / 2 + yc;
+                points.Add(new int[2] { xc, yc });
+                MidpointDisplacement(xb, yb, xc, yc, control);
+                MidpointDisplacement(xc, yc, xe, ye, control);
+            }
+
+        }
+
+        public class comparePoints : IComparer
+        {
+            int IComparer.Compare(Object x, Object y)
+            {
+                int[] p1 = (int[])x;
+                int[] p2 = (int[])y;
+                return p1[0].CompareTo(p2[0]);
+            }
+        }
+
+        private void DrawCurrentLine()
+        {
+            IComparer comp = new comparePoints();
+            points.Sort(comp);
+            Pen pen = new Pen(Color.Black);
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                int[] first = (int[])points[i];
+                int[] second = (int[])points[i + 1];
+                g.DrawLine(pen, first[0] + Width/2, Height/2 - first[1], second[0] + Width/2, Height/2 - second[1]);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            g = this.CreateGraphics();
+            rand = new Random();
+            points = new ArrayList();
+        }
+
+        private void DrawRocks_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            int xb = -Width / 2;
+            int yb = -100;
+            int xe = Width / 2;
+            int ye = 150;
+
+            points.Add(new int[2] { xb, yb });
+            points.Add(new int[2] { xe, ye });
+            MidpointDisplacement(xb, yb, xe, ye);
+
+            g.Clear(SystemColors.Control);
+            DrawCurrentLine();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            int xb = -Width / 2;
+            int yb = -100;
+            int xe = Width / 2;
+            int ye = 150;
+
+            points.Add(new int[2] { xb, yb });
+            points.Add(new int[2] { xe, ye });
+            MidpointDisplacementSteps(xb, yb, xe, ye);
+        }
+
+        void MidpointDisplacementSteps(int xb, int yb, int xe, int ye, int control = 0)
+        {
+            double l = Math.Sqrt((xe - xb) * (xe - xb) + (ye - yb) * (ye - yb));
+
+            g.Clear(SystemColors.Control);
+            DrawCurrentLine();
+            Thread.Sleep(500);
+
+            if (Width / 2 > Convert.ToInt32(Math.Pow(2, control)))
+            {
+                control++;
+                int xc = (xe + xb) / 2;
+                int yc = ((Height / 2 + yb) + (Height / 2 + ye)) / 2 +
+                         rand.Next(Convert.ToInt32(-l / R), Convert.ToInt32(l / R) + 1);
+                yc = -Height / 2 + yc;
+                points.Add(new int[2] { xc, yc });
+                MidpointDisplacementSteps(xb, yb, xc, yc, control);
+                MidpointDisplacementSteps(xc, yc, xe, ye, control);
+            }
+        }
+    }
+}
